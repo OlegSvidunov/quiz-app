a45485751983cf12b450c4843d276121aad813dd
Olga Meshchaninova <meshchaninova.olga@gmail.com>
2019-12-07 16:37:26 +0200

    add new quiz result entity

---
+++ blue-quiz/src/main/java/com/blueteam/bluequiz/entities/QuizResult.java
@@ -0,0 +1,17 @@
+package com.blueteam.bluequiz.entities;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class QuizResult {
+    private final Integer id;
+    private final Integer quizId;
+    private final String quizName;
+    private final Double result;
+    private LocalDateTime finishedTime;
+
+}
